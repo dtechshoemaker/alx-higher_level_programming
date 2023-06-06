ff386ea7f0eca7e9fc9b164061f4a4ad7d923109
peterkimbeghi <peterkimbeghi@gmail.com>
2023-06-06 03:39:44 -0700

    Write a Shell script that runs a Python script task done

--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,1 +1,2 @@
-# This is a readme file that contains all the project task in this directory of hello world
+# This is a readme file that contains all the project task in this directory of hello worl
+0. Task 0 done Write a Shell script that runs a Python script. 
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * struct lintint_s -singly linked list
+ * @n: integer
+ * @next: points to the node
+ *
+ * DEscription: singly list node structure
+ * for Alx project
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
