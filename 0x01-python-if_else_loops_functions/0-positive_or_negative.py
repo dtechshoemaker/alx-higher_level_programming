3ff749de7440ba93a424e38c06d2c50136ee2316
peterkimbeghi <peterkimbeghi@gmail.com>
2023-06-06 04:17:32 -0700

    Task 0 in 0x01 folder done

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+mport random
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative number!")
