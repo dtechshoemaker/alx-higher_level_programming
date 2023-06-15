b3fa98c7c49727f0ff35d6aba4d1e4dbda8a65f5
peterkimbeghi <peterkimbeghi@gmail.com>
2023-06-15 13:47:57 -0700

    Committed task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,3 +2,14 @@
 
 def square_matrix_simple(matrix=[]):
 
+    """
+    A function that returns and compute 
+    the square value of every integer in a matrix
+    """
+    newMatrix = []
+
+        for col in matrix:
+            result = list(map(lambda x: x**2, col))
+            newMatrix.append(result)
+        return newMatrix
+    
